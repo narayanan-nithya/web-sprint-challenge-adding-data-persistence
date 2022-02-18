b4fb2a63d1da5cb032e854f8fa3a56598b294a45
Nithya Narayanan <n.nithya@pm.me>
2022-02-17 22:46:15 -0800

    project model

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,28 @@
 // build your `Project` model here
+const db = require('../../data/dbConfig')
+
+const findById = async() => {
+    const projectStatus = await db('projects')
+
+    projectStatus.forEach(status => {
+        if(status.project_completed === 0){
+            status.project_completed = false
+        }else {
+            status.project_completed = true
+        }
+    })
+
+return projectStatus
+}
+
+const create = async (status) => {
+    const [id] = await db("projects").insert(status)
+    return db("projects")
+    .where("project_id", id)
+    .first()
+}
+
+module.exports = {
+    findById,
+    create
+}
--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,23 @@
 // build your `/api/projects` router here
+
+const express = require('express');
+
+const Project = require('./model.js');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    Project.findById()
+        .then((proejcts) => {
+            res.status(200).json(projects)
+        })
+        .catch(err => next(err));
+});
+
+router.post('/', (req, res, next) => {
+    Project.create(req.body)
+    .then(project => res.status(201).json(proejct))
+    .catch(err => next(err))
+})
+
+module.exports = router

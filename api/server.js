a6c0fed457ec980abfb69fd9af6e7b302bfee550
Nithya Narayanan <n.nithya@pm.me>
2022-02-17 22:18:11 -0800

    server.js file

--- api/server.js
+++ api/server.js
@@ -1,1 +1,16 @@
 // build your server here and require it from index.js
+const express = require('express');
+const server = express();
+
+
+server.use(express.json());
+const resourceRouter = require('./resource/router');
+const projectRouter = require ('./project/router');
+const taskRouter = require ('./task/router');
+
+server.use('/api/projects', projectRouter);
+server.use('/api/resources', resourceRouter);
+server.use('/api/tasks', taskRouter);
+
+module.exports = server;
+
